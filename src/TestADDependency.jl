af13fb144ad55c610be25f75f9485d7e3dc305b9
Adrian Hill <adrian.hill@mailbox.org>
2022-05-30 14:48:35 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/TestADDependency.jl
@@ -0,0 +1,5 @@
+module TestADDependency
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using TestADDependency
+using Test
+
+@testset "TestADDependency.jl" begin
+    # Write your tests here.
+end
